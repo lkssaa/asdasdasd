0bfd1a3882564c04bcfc6c6de63f3dcd986c70db
lkssaa <105158753+lkssaa@users.noreply.github.com>
2024-10-29 17:47:29 +0300

    Add files via upload

---
+++ lab2 1.cs
@@ -0,0 +1,123 @@
+using System;
+
+
+public class StringWithExclamation
+{
+    protected string value;
+
+
+    public StringWithExclamation(string str)
+    {
+        value = str;
+    }
+
+
+    public StringWithExclamation(StringWithExclamation obj)
+    {
+        value = obj.value;
+    }
+
+
+    public void AddExclamations()
+    {
+        value = "!!! " + value;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{value}";
+    }
+}
+
+
+public class ImportantString : StringWithExclamation
+{
+    private int priority;
+
+
+    public ImportantString(string str) : base(str)
+    {
+        priority = 0;
+    }
+    
+
+    public ImportantString(ImportantString obj) : base(obj)
+    {
+       priority = obj.priority;
+    }
+
+ 
+    public void RemoveExclamations()
+    {
+        value = value.TrimStart('!', '!', '!', ' ');
+    }
+    
+    
+    public void PriorityIncrease()
+    {
+        priority++;
+        AddExclamations();
+    }
+    
+    public void PriorityDecrease()
+    {
+        if (priority == 0) return;
+        priority--;
+        RemoveExclamations();
+    }
+
+
+    public override string ToString()
+    {
+        return $"{value} (важность: {priority})";
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("какая хотите строка:");
+        string a = Console.ReadLine();
+        var str1 = new StringWithExclamation(a);
+        Console.WriteLine($"конструктор по умолчанию - {str1.ToString()}");
+
+        var str2 = new StringWithExclamation(str1);
+        Console.WriteLine($"конструктор копий - {str2.ToString()}");
+        str2.AddExclamations();
+        Console.WriteLine($"метод для добавления восклицательных знаков - {str2.ToString()}");
+
+
+        Console.WriteLine("какая хотите строка:");
+        a = Console.ReadLine();
+        Console.WriteLine($"конструктор по умолчанию и метод уменьшения важности - {str1.ToString()}");
+        var extStr1 = new ImportantString(a);
+        extStr1.PriorityIncrease();
+        extStr1.PriorityDecrease();
+        Console.WriteLine(extStr1.ToString());
+
+        var extStr2 = new ImportantString(extStr1);
+        Console.WriteLine($"конструктор копий и метод увеличения важности - {str1.ToString()}");
+        extStr2.PriorityIncrease();
+        extStr2.PriorityIncrease();
+        extStr2.PriorityIncrease();
+        Console.WriteLine(extStr2.ToString());
+        
+        Console.WriteLine("какая хотите строка:");
+        a = Console.ReadLine();
+        var str3 = new ImportantString(a);
+        Console.WriteLine("+/-/exit:");
+        a = Console.ReadLine();
+        while (a!="exit"){
+            if (a == "+") str3.PriorityIncrease();
+            if (a == "-") str3.PriorityDecrease();
+            
+            Console.WriteLine(str3.ToString());
+            
+            Console.WriteLine("+/-/exit:");
+            a = Console.ReadLine();
+        }
+    }
+}
---
+++ lab2 2-3.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+public class Money
+{
+    private uint rubles;
+    private byte kopeks;
+
+    public Money(uint a, byte b)
+    {
+        rubles = (uint)(a + Convert.ToInt32(b) / 100);
+        kopeks = (byte)(Convert.ToInt32(b) % 100);
+
+    }
+
+    public static Money operator +(Money m1, uint m2)
+    {
+        uint totalRubles = m1.rubles + m2 / 100;
+        byte totalKopeks = (byte)(m1.kopeks + m2 % 100);
+
+        return new Money(totalRubles, totalKopeks);
+    }
+
+    public static Money operator +(uint m2, Money m1)
+    {
+        uint totalRubles = m1.rubles + m2 / 100;
+        byte totalKopeks = (byte)(m1.kopeks + m2 % 100);
+
+        return new Money(totalRubles, totalKopeks);
+    }
+
+    public static Money operator -(uint m2, Money m1)
+    {
+        long totalRubles = m2 / 100 - Convert.ToInt32(m1.rubles) ;
+        byte totalKopeks = (byte)(m2 % 100 - m1.kopeks);
+        if (totalRubles < 0) return new Money(0, 0);
+
+        return new Money((uint)totalRubles, totalKopeks);
+    }
+
+    public static Money operator -(Money m1, uint m2)
+    {
+        long totalRubles = Convert.ToInt32(m1.rubles) - m2 / 100;
+        byte totalKopeks = (byte)(m1.kopeks - m2 % 100);
+        if (totalRubles < 0)  return new Money(0,0); 
+
+        return new Money((uint)totalRubles, totalKopeks);
+    }
+
+
+    public static explicit operator uint(Money money)
+    {
+        return money.rubles;
+    }
+
+    public static implicit operator double(Money money)
+    {
+
+            return (double)((double)money.kopeks/100);
+    }
+
+    public static Money operator ++(Money m)
+    {
+        if (m.kopeks == 99)
+        {
+            m.rubles++;
+            m.kopeks = 0;
+        }
+        else
+        {
+            m.kopeks++;
+        }
+        return m;
+    }
+
+    public static Money operator --(Money m)
+    {
+        if (m.kopeks == 0)
+        {
+            m.rubles--;
+            m.kopeks = 99;
+        }
+        else
+        {
+            m.kopeks--;
+        }
+        return m;
+    }
+
+    public override string ToString()
+    {
+        return $"{rubles}.{kopeks:D2} руб.";
+    }
+
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int c, d;
+            uint a;
+            byte b;
+            Console.WriteLine("введите два числа - рубли и копейки:");
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            if (c < 0 || d < 0)
+            {
+                Console.WriteLine("некорректный ввод");
+                return;
+            }
+            a = (uint)c;
+            b = (byte)d;
+
+            Money money1 = new Money(a, b);
+
+            Console.WriteLine($"итого - {((uint)money1).ToString()} рублей {((double)money1).ToString()} рублей");
+
+            Console.WriteLine($"плюс копейка - {(money1++).ToString()}");
+            Console.WriteLine($"обратно минус копейка - {(money1--).ToString()}");
+
+            Console.WriteLine("введите копейки для добавления к текущей сумме:");
+
+            int a2 = Convert.ToInt32(Console.ReadLine());
+
+            if (a2 < 0)
+            {
+                Console.WriteLine("введите неотрицательное число");
+                return;
+            }
+
+            uint a1 = (uint)a2;
+
+            Console.WriteLine($"итого - {(money1 + a1).ToString()} = {(a1 + money1).ToString()}");
+            Console.WriteLine("вычитание того же числа копеек от текущей суммы:");
+
+
+
+            Console.WriteLine($"итого - {(money1 - a1).ToString()} или {(uint)(a1-money1)} (где-то здесь должен быть ноль, так как это проверка на отрицательные деньги так работает)");
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("некорректный ввод");
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("слишком большое значение");
+        }
+    }
+}
